df04139643c4e6dde71724aad71dbe548d1cc12f
Dldvlpr <dormoy.dev@gmail.com>
2022-04-05 10:14:48 +0200

    quete variable

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+$nameVO = "Indiana Jones and the Last Crusade";
+$watchedMovie = "true";
+$releaseInfo = 1989;
+$note = 8.3;
+
+    echo   $nameVO . ", " .  $watchedMovie . ", " . $releaseInfo . ", rating: " . $note . "/10";
