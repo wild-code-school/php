0dfb278bf7bb5371208938ccf317ad78e3d6e085
Dldvlpr <dormoy.dev@gmail.com>
2022-05-02 12:19:06 +0200

    quete finit

---
+++ config/twig.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$contactEmail = 'contact@gmail.com';
+
+$loader = new Twig\Loader\FilesystemLoader(__DIR__ . '/../src/View');
+$twig = new Twig\Environment($loader, ['debug' => true]);
+$twig->addExtension(new Twig\Extension\DebugExtension());
+
+$twig->addGlobal('contactEmail', $contactEmail);
---
+++ public/details.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once __DIR__ . '/../config/twig.php';
+
+echo $twig->render('detail.html.twig');
